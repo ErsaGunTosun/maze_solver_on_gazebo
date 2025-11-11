59761948a22438be93d682bc514b383b23dd9a26
ErsaGunTosun <ersagunfatihdev@gmail.com>
2025-11-11 16:49:29 +0000

    UPDATE bring up launch file bring up launch file map argument added

--- robot_simulation/launch/bring_up.launch.py
+++ robot_simulation/launch/bring_up.launch.py
@@ -3,22 +3,37 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    difficulty_arg = DeclareLaunchArgument(
+        'difficulty',
+        default_value='easy',
+        description='Difficulty level: easy, medium, or hard'
+    )
+    
+    difficulty = LaunchConfiguration('difficulty')
+
+    filename = [difficulty, TextSubstitution(text='.sdf')]
+
+    package_share_dir = get_package_share_directory("robot_simulation")
+
+    world_path = PathJoinSubstitution([
+        package_share_dir,
+        'worlds',
+        filename  
+    ])
+    
+
     ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
     launch_path = os.path.join(
             get_package_share_directory("robot_simulation"),
             "launch"
-    )
-    
-    world_path = os.path.join(
-            get_package_share_directory("robot_simulation"),
-            "worlds",
-            "empty_world.world"
     )
 
     gz_server = IncludeLaunchDescription(
@@ -51,6 +66,7 @@ def generate_launch_description():
 
 
     return LaunchDescription([
+        difficulty_arg,
         gz_server,
         gz_client,
         robot_state_publisher,
