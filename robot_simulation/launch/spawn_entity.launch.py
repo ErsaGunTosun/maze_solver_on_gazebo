a65aec9d5809e51a5e9d8472fca308ce85a80887
ErsaGunTosun <ersagunfatihdev@gmail.com>
2025-11-17 21:33:55 +0000

    UPDATE robot model tf tree robot tf tree connect odom

--- robot_simulation/launch/spawn_entity.launch.py
+++ robot_simulation/launch/spawn_entity.launch.py
@@ -32,9 +32,13 @@ def generate_launch_description():
             '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
             '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
             '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
+            '/model/robot/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
         ],
         output='screen',
-        parameters=[{'use_sim_time': True}]
+        parameters=[{'use_sim_time': True}],
+        remappings=[
+            ('/model/robot/tf', '/tf'), 
+        ]
     )
 
 
