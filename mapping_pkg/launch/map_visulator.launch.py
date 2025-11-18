fede0e9e3020f09842dfa3ad151035405a2ac6f2
ErsaGunTosun <ersagunfatihdev@gmail.com>
2025-11-18 17:55:15 +0000

    UPDATE mapping launch file mapping launch file added rviz node

--- mapping_pkg/launch/map_visulator.launch.py
+++ mapping_pkg/launch/map_visulator.launch.py
@@ -4,16 +4,24 @@ from ament_index_python.packages import get_package_share_directory
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     package_share_dir = get_package_share_directory("mapping_pkg")
 
-    config_file_path = os.path.join(
+    slam_config_file_path = os.path.join(
         package_share_dir,
         'config',
         'slam.yaml'
     )
+    
+    rviz_config_file_path = os.path.join(
+        package_share_dir,
+        'config',
+        'rviz_config.rviz'
+    )
+
 
     slam_pkg = get_package_share_directory("slam_toolbox")
 
@@ -23,11 +31,20 @@ def generate_launch_description():
             ),
             launch_arguments={
                 'use_sim_time': 'True',
-                'slam_params_file': config_file_path,
+                'slam_params_file': slam_config_file_path,
             }.items()
+    )
+
+    rviz = Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="screen",
+            arguments=["-d", rviz_config_file_path] 
     )
     
 
     return LaunchDescription([
-        slam
+        slam,
+        rviz
     ])
